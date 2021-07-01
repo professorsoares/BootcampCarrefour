72dbeed45ae599095b5817ec00cb4572f9c4b44f
seuLindoNome <seuLindoEmail>
2021-07-01 17:28:59 -0300

    Iniciando o Repositorio GIT

--- aula07.py
+++ aula07.py
@@ -1,8 +1,49 @@
 
 
+class Calculadora:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-def soma(a, b):
-    return a + b
+    def soma(self):
+        return self.a + self.b
 
-print(soma(1,2))
-print(soma(3,4))
+    def subtracao(self):
+        return self.a - self.b
+
+    def divisao(self):
+        return self.a / self.b
+
+    def multiplicacao(self):
+        return self.a * self.b
+
+
+calc = Calculadora(10,2)
+
+print(calc.soma())
+print(calc.subtracao())
+print(calc.divisao())
+print(calc.multiplicacao())
+
+
+
+#--------------------------------------
+
+#
+# def soma(a, b):
+#     return a + b
+#
+# def subtracao(a, b):
+#     return a - b
+#
+# def divisao(a, b):
+#     return a / b
+#
+# def multiplicacao(a, b):
+#     return a * b
+
+# print(soma(1,2))
+# print(soma(3,4))
+# print(subtracao(3,4))
+# print(divisao(3,4))
+# print(multiplicacao(3,4))
---
+++ aula07_1.py
@@ -0,0 +1,18 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+
+    def power(self):
+        if self.ligada:
+            self.ligada = False
+        else:
+            self.ligada = True
+    def aumenta_canal
+tv = Televisao()
+print(tv.ligada)
+
+tv.power()
+print(tv.ligada)
+
+tv.power()
+print(tv.ligada)
